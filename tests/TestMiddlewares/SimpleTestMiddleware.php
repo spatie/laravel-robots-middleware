d09b21bd74307f951b2a4e05243529fddeae35dd
Rias <hello@rias.be>
2020-03-03 08:28:34 +0000

    Apply fixes from StyleCI

--- tests/TestMiddlewares/SimpleTestMiddleware.php
+++ tests/TestMiddlewares/SimpleTestMiddleware.php
@@ -7,7 +7,7 @@ use Spatie\RobotsMiddleware\RobotsMiddleware;
 
 class SimpleTestMiddleware extends RobotsMiddleware
 {
-    protected function shouldIndex(Request $request) : bool
+    protected function shouldIndex(Request $request): bool
     {
         return $request->segment(1) !== 'go-away';
     }
